b914aef18f7f92711162ab2ee4fd0c2c719c151d
srikanthgorrepatisccm <147691284+srikanthgorrepatisccm@users.noreply.github.com>
2023-11-02 11:23:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
